ddd2bc8bc6c8e7c33fbaea069449e277a1043d64
Mateusz Róg <73105134+Milkilej@users.noreply.github.com>
2021-09-22 14:41:45 +0200

    Add files via upload

---
+++ stringLib.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <iosfwd>
+
+namespace utils
+{
+
+class StringLib
+{
+    StringLib() = delete;
+    StringLib(const StringLib&) = delete;
+    StringLib& operator=(const StringLib&) = delete;
+    StringLib& operator=(StringLib&&) = delete;
+    ~StringLib() = delete;
+
+public:
+    static std::string spacesToUnderscoresRemoveNumbers(std::string input);
+    static const char* errorString(unsigned errorCode);
+    static bool sameStrings(std::string str1, std::string str2);
+};
+    
+}
